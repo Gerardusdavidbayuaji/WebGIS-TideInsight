fbe8e5076bad92147e8b463a694dd05e6fc5175a
Gerardusdavidbayuaji <gerardusdavidbayu@gmail.com>
2024-01-08 21:48:51 +0700

    feat: folder initialisation

---
+++ webgis-rob/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const Index = () => {
+  return (
+    <div>
+      <p className="text-3xl font-bold underline">HALLO SEMUANYA</p>
+      <Button>klik</Button>
+    </div>
+  );
+};
+
+export default Index;
